3ec201b1d38e1fd11c05e59de4a5f7bae2532f0e
kostaskara21 <kostas.kara213@gmail.com>
2025-02-21 00:37:11 +0100

    Finish Basic Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   resources :todos do
     resources :items
   end
+
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
+
   # root "posts#index"
 end
